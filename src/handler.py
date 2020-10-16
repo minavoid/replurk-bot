4137a59d093df400a62aeee9931a9670911abdfa
minavoid <JungHsuan@soe.ucsc.edu>
2020-10-16 18:08:21 +0900

    feat: search key word and replurk

--- src/handler.py
+++ src/handler.py
@@ -5,9 +5,28 @@ from dotenv import load_dotenv
 
 def hello(event, context):
     load_dotenv()
-    plurk = PlurkAPI(os.environ['PLURK_APP_KEY'], os.environ['PLURK_SECRET'])
-    plurk.authorize(os.environ['PLURK_ACCESS_TOKEN'], os.environ['PLRUK_ACCESS_SECRET'])
-    print(plurk.callAPI('/APP/Users/me'))
+    plurkAPI = PlurkAPI(os.environ['PLURK_APP_KEY'], os.environ['PLURK_SECRET'])
+    plurkAPI.authorize(os.environ['PLURK_ACCESS_TOKEN'], os.environ['PLRUK_ACCESS_SECRET'])
+
+    query = os.environ['QUERY']
+    max_size = os.environ['MAX_SIZE']
+    offset = 0
+    while offset < max_size:
+        results = plurkAPI.callAPI('/APP/PlurkSearch/search',
+                                   options={'query': query, 'offset' : offset});
+        filtered = list(filter(sould_replurk , results['plurks']))
+        if not filtered:
+            break
+        plurk_ids =  list(map(lambda x : x['plurk_id'], filtered))
+        #print(plurk_ids)
+        plurkAPI.callAPI('/APP/Timeline/replurk',
+                         options={'ids': json.dumps(plurk_ids)})
+        offset += results['last_offset']
+
+
+def sould_replurk(data) -> bool:
+    return data['replurkable'] and not data['replurked']
+
 
 if __name__ == "__main__":
     hello('', '')
